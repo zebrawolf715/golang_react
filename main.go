195ceb8469cfe25e3ab1fada004e2620138a8158
zebrawolf <zebrawolfweb@gmail.com>
2024-06-30 11:41:31 +0900

    add

--- main.go
+++ main.go
@@ -8,8 +8,12 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello worlds ")
+	fmt.Println("Hello world ")
 	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"msg": "hello world"})
+	})
 
 	log.Fatal(app.Listen(":4000"))
 
