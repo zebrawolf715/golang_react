9ebbbf5eb17a60a667959eebf3880f0d853e6eeb
zebrawolf <zebrawolfweb@gmail.com>
2024-06-30 12:04:56 +0900

    add

--- main.go
+++ main.go
@@ -7,12 +7,25 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type Todo struct {
+	ID        int    `json:"id"`
+	Completed bool   `json:"completed"`
+	Body      string `json:"body"`
+}
+
 func main() {
 	fmt.Println("Hello world ")
 	app := fiber.New()
 
+	todos := []Todo{}
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Status(200).JSON(fiber.Map{"msg": "hello world"})
+	})
+
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := Todo{} // {id:0, completed:false, body:""}
+
 	})
 
 	log.Fatal(app.Listen(":4000"))
