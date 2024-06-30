9003191f4ca58e19d3fa47305d1c06cdd6fb49e7
zebrawolf <zebrawolfweb@gmail.com>
2024-06-30 10:25:17 +0900

    Add

--- main.go
+++ main.go
@@ -1,8 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	fmt.Println("Hello world")
+	app := fiber.New()
+
+	log.Fatal(app.Listen(":4000"))
 
 }
