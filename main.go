4747d24f85982e8d941bb34f6baf8ee2c301ad14
zebrawolf <zebrawolfweb@gmail.com>
2024-07-02 01:30:30 +0900

    Add Patch

--- main.go
+++ main.go
@@ -117,7 +117,23 @@ func createTodo(c *fiber.Ctx) error {
 
 // Patch
 func updateTodo(c *fiber.Ctx) error {
+	id := c.Params("id")
+	objectID, err := primitive.ObjectIDFromHex(id)
 
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "Invalid todo ID"})
+	}
+
+	filter := bson.M{"_id": objectID}
+	update := bson.M{"$set": bson.M{"completed": true}}
+
+	_, err = collection.UpdateOne(context.Background(), filter, update)
+
+	if err != nil {
+		return err
+	}
+
+	return c.Status(200).JSON(fiber.Map{"success": true})
 }
 
 //func deleteTodo(c *fiber.Ctx) error {}
